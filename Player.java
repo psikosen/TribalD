5c74a2e252fa135749245cd67100372a144bc41f
psikosen <raymondgonzalez22@gmail.com>
2015-06-02 21:18:49 -0400

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,22 @@
+package Calandar;
+
+public class Player extends Entity{
+	
+		public boolean isAttacking = false;
+		public boolean dead = false;
+		PlayerStats player;
+		
+		Player(PlayerStats player, boolean dead){
+			
+			
+		}
+		
+		
+		@Override
+		public void update(float data) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		
+}
