f4149ceab270f0391332797b3555dc6c2c8dddb3
psikosen <raymondgonzalez22@gmail.com>
2015-06-02 21:17:53 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.example.tribald;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.act_main);
+	}
+}
