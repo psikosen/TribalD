6bdea131490ca574f2b30e0af520bef2e57cde63
psikosen <raymondgonzalez22@gmail.com>
2015-06-02 22:15:55 -0400

    OTile

---
+++ Otile.java
@@ -0,0 +1,68 @@
+package Calandar;
+
+/**
+ * Created by Rook on 5/30/2015.
+ */
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrthogonalTiledMapRendererWithSprites extends OrthogonalTiledMapRenderer {
+    private Sprite sprite;
+    private List<Sprite> sprites;
+    private int drawSpritesAfterLayer = 1;
+
+    public OrthogonalTiledMapRendererWithSprites(TiledMap map) {
+        super(map);
+        sprites = new ArrayList<Sprite>();
+    }
+
+  /*  public OrthogonalTiledMapRendererWithSprites(TiledMap map) {
+        super(map);
+    }*/
+
+   /* @Override
+    public void renderObject(MapObject object) {
+        if(object instanceof TextureMapObject) {
+            TextureMapObject textureObj = (TextureMapObject) object;
+            spriteBatch.draw(textureObj.getTextureRegion(), textureObj.getX(), textureObj.getY());
+        }
+    }*/
+    public void addSprite(Sprite sprite){
+        sprites.add(sprite);
+    }
+
+    @Override
+    public void render() {
+        beginRender();
+        int currentLayer = 0;
+        for (MapLayer layer : map.getLayers()) {
+            if (layer.isVisible()) {
+                if (layer instanceof TiledMapTileLayer) {
+                    renderTileLayer((TiledMapTileLayer)layer);
+                    currentLayer++;
+                    if(currentLayer == drawSpritesAfterLayer){
+                        for(Sprite sprite : sprites)
+                            sprite.draw(this.getBatch());
+                    }
+                } else {
+                    for (MapObject object : layer.getObjects()) {
+                        renderObject(object);
+                    }
+                }
+            }
+        }
+        endRender();
+    }
+
+
+}
+
+OT
