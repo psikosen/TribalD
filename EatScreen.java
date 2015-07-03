ddb439429ef0fb80f95b66b4c4395c2aeeb46401
psikosen <raymondgonzalez22@gmail.com>
2015-07-03 15:31:58 -0400

    Create EatScreen.java

---
+++ EatScreen.java
@@ -0,0 +1,25 @@
+package Calandar;
+
+/**
+ * Created by Rook on 6/18/2015.
+ */
+public class EatScreen {
+
+
+  /*  public EatScreen(Creature player) {
+        super(player);
+    }
+
+    protected String getVerb() {
+        return "eat";
+    }
+
+    protected boolean isAcceptable(Item item) {
+        return item.foodValue() != 0;
+    }
+
+    protected Screen use(Item item) {
+        player.eat(item);
+        return null;
+    }*/
+}
