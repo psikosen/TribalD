fe367e4fa36da2179238d80a58f94cd1271a3b99
psikosen <raymondgonzalez22@gmail.com>
2015-07-03 15:38:27 -0400

    Create PlayScreen.java

---
+++ PlayScreen.java
@@ -0,0 +1,19 @@
+package Calandar;
+
+/**
+ * Created by Rook on 6/18/2015.
+ */
+public class PlayScreen {
+            private World world;
+            private int centerX;
+            private int centerY;
+            private int ScreenWidth;
+            private int ScreenHight;
+
+                public PlayScreen(){
+
+
+                }
+
+
+}
