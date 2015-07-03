8280a6efe998a2313cee54e26d437f9257844217
psikosen <raymondgonzalez22@gmail.com>
2015-07-03 15:43:57 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,3 +13,25 @@ public class Main extends Activity {
 		setContentView(R.layout.act_main);
 	}
 }
+
+
+///
+package com.mygdx.game.android;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+/**
+ * Created by Rook on 5/30/2015.
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        LwjglApplicationConfiguration cfg = new
+                LwjglApplicationConfiguration();
+        cfg.title = "demo";
+        cfg.width = 480;
+        cfg.height = 320;
+     //   new LwjglApplication(new AndroidLauncher(), cfg);
+    }
+}
+
