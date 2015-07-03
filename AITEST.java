db830ed9e61d0ae3e4daf006273fa18f07712a13
psikosen <raymondgonzalez22@gmail.com>
2015-07-03 15:29:45 -0400

    Create AITEST.java

---
+++ AITEST.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Rook on 6/17/2015.
+ */
+public class AITEST extends  CreatureAi {
+
+
+    public AITEST(Creature creature, Creature player) {
+        super(creature, player);
+    }
+
+    public AITEST(Creature creature) {
+        super(creature);
+    }
+}
