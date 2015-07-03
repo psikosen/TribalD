c1e9bacf6dd01e5a22523af01637fd2e5350e55a
psikosen <raymondgonzalez22@gmail.com>
2015-07-03 15:28:35 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -8,6 +8,8 @@ public class Item {
         Image item;
         int id;
         boolean Used;
+        private double width;
+        private double height;
 
     public Item(String name, String description, Image item, int id, boolean used) {
         this.name = name;
@@ -17,4 +19,27 @@ public class Item {
         Used = used;
     }
     Item(){}
+
+
+    public void addAtEmptyLocation(Item item, int depth) {
+        int x;
+        int y;
+/*
+        do {
+            x = (int)(Math.random() * width);
+            y = (int)(Math.random() * height);
+      }
+       while (!tile(x,y,depth).isGround() || item(x,y,depth) != null);
+
+        items[x][y][depth] = item;
+    /* 
+     * 
+    }
+     */
+    }
+        
+	public Object foodValue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
