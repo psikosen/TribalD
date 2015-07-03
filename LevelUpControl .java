a801bc7bfadeb097ce7e7f1b9e122706fd9076c4
psikosen <raymondgonzalez22@gmail.com>
2015-07-03 15:34:30 -0400

    Create LevelUpControl .java

---
+++ LevelUpControl .java
@@ -0,0 +1,24 @@
+/**
+ * Created by Rook on 6/18/2015.
+ */
+public class LevelUpControl {
+
+   /* private static LevelUpOption[] options = new LevelUpOption[]{
+            new LevelUpOption("Increased hit points"){
+                public void invoke(Creature creature) { creature.gainMaxHp(); }
+            },
+            new LevelUpOption("Increased attack value"){
+                public void invoke(Creature creature) { creature.gainAttackValue(); }
+            },
+            new LevelUpOption("Increased defense value"){
+                public void invoke(Creature creature) { creature.gainDefenseValue(); }
+            },
+            new LevelUpOption("Increased vision"){
+                public void invoke(Creature creature) { creature.gainVision(); }
+            }
+    };*/
+/*
+    public void autoLevelUp(Creature creature){
+        options[(int)(Math.random() * options.length)].invoke(creature);
+    }*/
+}
