01677a86da289e580dc186fbefb7846055a97b60
Rook <raymondgonzalez22@gmail.com>
2016-03-18 15:43:23 -0400

    Update Disease.java

--- Disease.java
+++ Disease.java
@@ -7,25 +7,21 @@ public class Disease {
             int numOfDaysInfected;
             double chanceOfDying;
 
-    public Disease(String name, Calandar.Severity[] severity, int numOfDaysInfected, double chanceOfDying) {
-        this.name = name;
-        Severity = severity;
-        this.numOfDaysInfected = numOfDaysInfected;
-        this.chanceOfDying = chanceOfDying;
-        InfectionRATE(numOfDaysInfected);
-        
-            }
-
-    Disease(Player p){
-                        p.infect();
-
-
+            public Disease(String name, Calandar.Severity[] severity, int numOfDaysInfected, double chanceOfDying) {
+                    this.name = name;
+                    Severity = severity;
+                    this.numOfDaysInfected = numOfDaysInfected;
+                    this.chanceOfDying = chanceOfDying;
+                    InfectionRATE(numOfDaysInfected);
+                        }
+            Disease(Player p){
+                    p.infect();
             }
             void infect() {
                         
                         
             }           
-    public void InfectionRATE(int nod){
+            public void InfectionRATE(int nod){
          /*    switch(nod){
                   case nod < 10 :
                      chaceOfDying = chaceOfDying *= (10/100) ;                  
@@ -42,43 +38,38 @@ public class Disease {
                        break;
                      default:
                           chaceOfDying = 0 ;                  
-                    
                        
         }*/
-        
-   
     }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Calandar.Severity[] getSeverity() {
-        return Severity;
-    }
-
-    public void setSeverity(Calandar.Severity[] severity) {
-        Severity = severity;
-    }
-
-    public int getNumOfDaysInfected() {
-        return numOfDaysInfected;
-    }
-
-    public void setNumOfDaysInfected(int numOfDaysInfected) {
-        this.numOfDaysInfected = numOfDaysInfected;
-    }
-
-    public double getChanceOfDying() {
-        return chanceOfDying;
-    }
-
-    public void setChanceOfDying(double chanceOfDying) {
-        this.chanceOfDying = chanceOfDying;
-    }
-}
+                public String getName() {
+                    return name;
+                }
+            
+                public void setName(String name) {
+                    this.name = name;
+                }
+            
+                public Calandar.Severity[] getSeverity() {
+                    return Severity;
+                }
+            
+                public void setSeverity(Calandar.Severity[] severity) {
+                    Severity = severity;
+                }
+            
+                public int getNumOfDaysInfected() {
+                    return numOfDaysInfected;
+                }
+            
+                public void setNumOfDaysInfected(int numOfDaysInfected) {
+                    this.numOfDaysInfected = numOfDaysInfected;
+                }
+            
+                public double getChanceOfDying() {
+                    return chanceOfDying;
+                }
+            
+                public void setChanceOfDying(double chanceOfDying) {
+                    this.chanceOfDying = chanceOfDying;
+                }
+            }
